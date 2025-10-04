f0cacbd7170ab5917cd8ff71cca349828244d5cd
Shripad Maddi <stmaddi@outlook.com>
2025-10-04 21:01:26 +0530

    message

--- Terraform-Project/02-ec2/ec2.tf
+++ Terraform-Project/02-ec2/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "main" {
-  ami           = "ami-005e54dee72cc1d00" # us-west-2
+  ami           = "ami-0fcc78c828f981df2" # N.Virginia AMI
   instance_type = "t2.micro"
 
     tags = {
