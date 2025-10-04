277069cc1a15ed273877b91282ec30b39c81107b
Shripad Maddi <stmaddi@outlook.com>
2025-10-04 21:59:34 +0530

    message

--- Terraform-Project/03-vars/vars.tf
+++ Terraform-Project/03-vars/vars.tf
@@ -33,7 +33,7 @@ variable "sample_map" {
 
 
 #this is how we can declare output variable
-output "sample.map_output_x" {  
+output "sample.map" {  
   value = "${var.sample_map["name"]} is a ${var.sample_map["type"]} and his salary is ${var.sample_map["salary"]}"
 }
 
