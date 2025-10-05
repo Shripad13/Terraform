df11f9901811a4937903f83b5c9af83b21fb2578
Shripad Maddi <stmaddi@outlook.com>
2025-10-05 11:35:42 +0530

    message

--- Terraform-Project/06-loops-counts/main.tf
+++ Terraform-Project/06-loops-counts/main.tf
@@ -1,7 +1,19 @@
+# Iteration (0,1,2,3,4,..) number is called as count.index
+
+
 resource "null_resource" "main"  {
-  count = 5                   # count goes by list each & every iteration is defined by a index number
+  count = 3                   # count goes by list each & every iteration is defined by a index number
+}
+
+resource "null_resource" "fruits"  {
+  count = length(var.fruits)                         # here count should be based on no. of fruits declared in variable fruits 
+                                 
+}
+
+# length function gives the length of the list declared in variable fruits.
+
+variable "fruits" {
+  default = ["apple", "banana", "lemon"]
 }
 
 
-
-# Iteration (0,1,2,3,4,..) number is called as count.index
