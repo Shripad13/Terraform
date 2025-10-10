25b9644672a6121843f93f1f8862204964d1c9f3
Shripad Maddi <stmaddi@outlook.com>
2025-10-10 20:06:38 +0530

    message

--- Terraform-Project/expense-v6/main.tf
+++ Terraform-Project/expense-v6/main.tf
@@ -10,20 +10,5 @@ data "aws_security_group" "main" {
 # Outputs to display Security Group details
 output "security_group_id" {
   description = "The ID of the security group"
-  value       = data.aws_security_group.main.id
-}
-
-output "security_group_name" {
-  description = "The name of the security group"
-  value       = data.aws_security_group.main.name
-}
-
-output "security_group_description" {
-  description = "The description of the security group"
-  value       = data.aws_security_group.main.description
-}
-
-output "security_group_vpc_id" {
-  description = "The VPC ID where the security group is located"
-  value       = data.aws_security_group.main.vpc_id
+  value       = data.aws_security_group.main
 }
