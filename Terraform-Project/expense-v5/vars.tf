d1a2d566adfe0698bd310274fac5a47b857dd599
Shripad Maddi <stmaddi@outlook.com>
2025-10-10 19:32:29 +0530

    message

--- Terraform-Project/expense-v5/vars.tf
+++ Terraform-Project/expense-v5/vars.tf
@@ -29,9 +29,9 @@ variable "vpc_security_group_ids" {
 #}
 
 
-data "aws_security_group" "main" {
-  id = var.vpc_security_group_ids
-}
+#data "aws_security_group" "main" {
+#  id = var.vpc_security_group_ids
+#}
 
 output "security_group_id" {
   value = data.aws_security_group.main.id
--- Terraform-Project/expense-v6/main.tf
+++ Terraform-Project/expense-v6/main.tf
@@ -1,6 +1,10 @@
-data "aws_security_group" "main" {
-  id = var.vpc_security_group_ids
+
+variable "vpc_security_group_ids" {
+  description = "The ID of the VPC security group"
+  default = ["sg-082319ecdb6b861c8"]
+  type        = string
 }
+
 
 output "security_group_id" {
   value = data.aws_security_group.main.id
