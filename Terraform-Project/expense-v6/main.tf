8c3eea1ed238fd13c5990930e5f68db0bdf6685c
Shripad Maddi <stmaddi@outlook.com>
2025-10-10 19:59:15 +0530

    message

--- Terraform-Project/expense-v6/main.tf
+++ Terraform-Project/expense-v6/main.tf
@@ -1,9 +1,29 @@
+provider "aws" {
+  region = "us-east-1" # Update this to your desired AWS region
+}
 
+# Data source to fetch the existing Security Group
 data "aws_security_group" "main" {
-  id = var.vpc_security_group_ids[0]
+  id = var.vpc_security_group_id
 }
 
+# Outputs to display Security Group details
 output "security_group_id" {
-  value = data.aws_security_group.main.id
+  description = "The ID of the security group"
+  value       = data.aws_security_group.main.id
 }
 
+output "security_group_name" {
+  description = "The name of the security group"
+  value       = data.aws_security_group.main.name
+}
+
+output "security_group_description" {
+  description = "The description of the security group"
+  value       = data.aws_security_group.main.description
+}
+
+output "security_group_vpc_id" {
+  description = "The VPC ID where the security group is located"
+  value       = data.aws_security_group.main.vpc_id
+}
---
+++ Terraform-Project/expense-v6/variables.tf
@@ -0,0 +1,7 @@
+variable "vpc_security_group_id" {
+  description = "The ID of the existing VPC Security Group to fetch"
+  type        = string
+  default     = "sg-082319ecdb6b861c8" # Replace with your actual SG ID
+}
+
+
