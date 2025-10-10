ec140ccfdacba9a6dda1bae97b9b9301d84e1fea
Shripad Maddi <stmaddi@outlook.com>
2025-10-10 19:19:40 +0530

    message

--- Terraform-Project/expense-v5/app/main.tf
+++ Terraform-Project/expense-v5/app/main.tf
@@ -4,7 +4,7 @@
 
 resource "aws_instance" "main" {
   ami           = var.ami 
-  instance_type = var.instance.type
+  instance_type = var.instance_type
   vpc_security_group_ids = var.vpc_security_group_ids
 
   tags = {
