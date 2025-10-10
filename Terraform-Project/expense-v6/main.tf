9030dcbeef51951be190d837ec0ffdb83ceb1240
Shripad Maddi <stmaddi@outlook.com>
2025-10-10 20:17:20 +0530

    message

--- Terraform-Project/expense-v6/main.tf
+++ Terraform-Project/expense-v6/main.tf
@@ -1,3 +1,6 @@
+# to display all available details of an existing security group in one output block
+
+
 provider "aws" {
   region = "us-east-1" # Update this to your desired AWS region
 }
@@ -7,8 +10,31 @@ data "aws_security_group" "main" {
   id = var.vpc_security_group_id
 }
 
+#output the entire data.aws_security_group.main object.
 # Outputs to display Security Group details
 output "security_group_id" {
   description = "The ID of the security group"
   value       = data.aws_security_group.main
+}
+
+
+# Outputs to display Security Group details for individual attributes
+output "security_group_id" {
+  description = "The ID of the security group"
+  value       = data.aws_security_group.main.id
+}
+
+output "security_group_name" {
+  description = "The name of the security group"
+  value       = data.aws_security_group.main.name
+}
+
+output "security_group_description" {
+  description = "The description of the security group"
+  value       = data.aws_security_group.main.description
+}
+
+output "security_group_vpc_id" {
+  description = "The VPC ID where the security group is located"
+  value       = data.aws_security_group.main.vpc_id
 }
