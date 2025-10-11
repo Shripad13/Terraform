dbc7aee2f7ea43ee3318c21d2b9aa8b05472c127
Shripad Maddi <stmaddi@outlook.com>
2025-10-11 13:44:29 +0530

    message

--- Terraform-Project/expense-v5/app/main.tf
+++ Terraform-Project/expense-v5/app/main.tf
@@ -29,10 +29,10 @@ resource "aws_route53_record" "main" {
 resource "null_resource" "app" {
   depends_on = [aws_route53_record.main, aws_instance.main]
 
-  triggers = {
+  triggers = {                      # This is to ensure that the provisioner runs every time
     always_run = true
   }
   provisioner "local-exec" {
-    command = "sleep 30; cd /home/ec2-user/Ansible ; ansible-playbook -i inv-dev  -e ansible_user=ec2-user -e ansible_password=DevOps321 -e COMPONENT=${var.name} -e ENV=dev -e PWD=${var.pwd} expense.yml"
+    command = "sleep 50; cd /home/ec2-user/Ansible ; ansible-playbook -i inv-dev  -e ansible_user=ec2-user -e ansible_password=DevOps321 -e COMPONENT=${var.name} -e ENV=dev -e PWD=${var.pwd} expense.yml"
   }
 }
